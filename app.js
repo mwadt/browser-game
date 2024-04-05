71c246bd0cae8b0b7599646f673ebeabab0ba8ea
mwadt <mbrincko93@gmail.com>
2024-04-05 12:56:02 -0700

    added absolute positioning to the div and commented out the original img in html

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const theRightSide = document.getElementById('rightSide');
 
 // function to generate flames on the left and right sides of the web page.
 const generateFlames = () => {
-    for(i = 1; i < numberOfFlames; i++) {
+    for(i = 0; i < numberOfFlames; i++) {
         let newFlame = document.createElement('img');
         newFlame.src = './image-folder/fire emoji.png';
         
@@ -16,15 +16,19 @@ const generateFlames = () => {
 
         const leftSideImages = theLeftSide.cloneNode(true);  // clones the left side flames to a new variable
         leftSideImages.removeChild(leftSideImages.lastChild); // removes the last child from the left side
-        leftSideImages.appendChild(theRightSide); // cop
+        theRightSide.appendChild(leftSideImages); // cop
     } 
         theLeftSide.lastElementChild.addEventListener('click', nextLevel); //adds an event listener for when the user clicks on the extra flame
+        document.body.addEventListener('click', gameOver);
    }
-// function to 
+//function to generate more flames when the user clicks the missing flame
 const nextLevel = (event) => {
     event.stopPropagation();
     numberOfFlames += 5;
     generateFlames();
 }
 
+const gameOver = () => {
+    alert('Game Over!');
+}
 
